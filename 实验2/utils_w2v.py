0654c25f67550525c6b24e9e0a22b2574aa5c31a
CandyMonster37 <76767579+CandyMonster37@users.noreply.github.com>
2021-04-22 10:23:14 +0800

    实验2的第一部分，w2v实现文本分类

---
+++ 实验2/utils_w2v.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+from gensim.models import word2vec, Word2Vec
+from tqdm import tqdm
+import numpy as np
+import jieba.analyse
+import os
+import re
+
+
+class TASK:
+    def __init__(self, root_path='./data'):
+        self.allPOS = ('n', 'ns', 'v', 'vn')
+        self.root_path = root_path
+        self.class_list = os.listdir(self.root_path)
+        self.all_txt_list = []
+        self.model = None
+        self.class_all_words = {}
+
+    def get_corpus(self):
+        for item in self.class_list:
+            class_path = self.root_path + '/' + item
+            file_list = os.listdir(class_path)
+            for file_ in file_list:
+                filename = class_path + '/' + file_
+                with open(filename, 'r', encoding='utf8') as f:
+                    cont = f.read()
+                cont = "".join(cont.split())
+                word_list = jieba.analyse.textrank(cont, topK=None, withWeight=False, allowPOS=('n', 'ns', 'v', 'vn'))
+                if len(word_list) == 0:
+                    continue
+                else:
+                    self.all_txt_list.extend(word_list)
+
+        result = " ".join(self.all_txt_list)
+        with open('result.txt', 'w', encoding='utf8') as f:
+            f.write(result)
+
+    def get_model(self):
+        if os.path.exists('my_model.model'):
+            self.model = Word2Vec.load('my_model.model')
+            return
+        sentences = word2vec.Text8Corpus("result.txt")
+        model = word2vec.Word2Vec(sentences, size=200, min_count=1)
+        model.save('my_model.model')
+        self.model = model
+
+    def train_frequency(self, rate_train):
+        print('\n对训练集词频统计')
+        for c in tqdm(self.class_list):
+            all_words = {}
+            class_path = self.root_path + '/' + c
+            file_list = os.listdir(class_path)
+            nFile = len(file_list)
+            for name in file_list[:int(nFile * rate_train)]:
+                file_path = class_path + '/' + name
+                with open(file_path, 'r', encoding='utf8') as f:
+                    cont = f.read()
+                    cont = re.sub(re.compile(r'原文网址:.*'), '', cont)
+                    cont = "".join(cont.split())
+                word_list = jieba.analyse.textrank(cont, topK=None, withWeight=False, allowPOS=self.allPOS)
+                if len(word_list) != 0:
+                    for word in word_list:
+                        if word in all_words.keys():
+                            all_words[word] += 1
+                        else:
+                            all_words[word] = 1
+            self.class_all_words[c] = all_words
+
+    def average_class_vector(self, keyword_num):
+        """
+        获取每个类的平均向量
+        """
+        average_class_dic = {}
+        for c in self.class_all_words:
+            all_words_list = sorted(self.class_all_words[c].items(), key=lambda item: item[1], reverse=True)
+            shape_ = self.model.wv[all_words_list[0][0]].shape
+            total = np.zeros(shape_[0])
+            limit = min(keyword_num, len(all_words_list))
+            for t in range(limit):
+                total += self.model.wv[all_words_list[t][0]]
+            average_class_dic[c] = total / limit
+        return average_class_dic
+
+    def acc(self, keyword_num, rate, average_class_dic):
+        true = 0
+        false = 0
+        print('\nKeyword_num: {}'.format(keyword_num))
+        for c in tqdm(self.class_list):
+            class_path = self.root_path + '/' + c
+            file_list = os.listdir(class_path)
+            nFile = len(file_list)
+            # 取30%作测试集
+            for name in file_list[int(nFile * rate):]:
+                file_path = class_path + '/' + name
+                test_data_words = {}
+                with open(file_path, 'r', encoding='utf8') as f:
+                    txt = f.read()
+                    txt = re.sub(re.compile(r'原文网址:.*'), '', txt)
+                txt = "".join(txt.split())
+                word_list = jieba.analyse.textrank(txt, topK=None, withWeight=False, allowPOS=('n', 'ns', 'v', 'vn'))
+                for word in word_list:
+                    if word in test_data_words.keys():
+                        test_data_words[word] += 1
+                    else:
+                        test_data_words[word] = 1
+                test_words_list = sorted(test_data_words.items(), key=lambda item: item[1], reverse=True)
+                if len(test_words_list) == 0:
+                    continue
+                shape_ = self.model.wv[test_words_list[0][0]].shape
+                total = np.zeros(shape_[0])
+                limit = min(keyword_num, len(test_words_list))
+                for t in range(limit):
+                    total += self.model.wv[test_words_list[t][0]]
+                average_test_vector = total / limit
+                pre = predict(average_class_dic, data=average_test_vector)
+                if pre == c:
+                    true += 1
+                else:
+                    false += 1
+        return true / (true + false)
+
+
+def predict(average_class_dic, data):
+    sim = {}
+    for c in average_class_dic:
+        sim[c] = cos_sim(data, average_class_dic[c])
+    test_word_list = sorted(sim.items(), key=lambda item: item[1], reverse=True)
+    return test_word_list[0][0]
+
+
+def cos_sim(vector_a, vector_b):
+    a_norm = np.linalg.norm(vector_a)
+    b_norm = np.linalg.norm(vector_b)
+    sim = np.dot(vector_a, vector_b) / (a_norm * b_norm)
+    return sim
---
+++ 实验2/w2v.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from utils_w2v import TASK
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import torch
+
+
+def main():
+    if not os.path.exists('./data'):
+        print('No source data file!')
+        print("Need folder named 'data' unzipped from 'data.zip'!")
+        return
+
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.rcParams['axes.unicode_minus'] = False
+    np.random.seed(100)
+    torch.cuda.manual_seed(100)
+    rate = 0.7
+    keyword_num_list = [1, 3, 5, 10, 15, 20, 30, 50, 100]
+
+    tar = TASK()
+    tar.get_corpus()
+    tar.get_model()
+    tar.train_frequency(rate)
+
+    print('\n计算准确率')
+    acc_list = []
+    for keyword_num in keyword_num_list:
+        average_class_dic = tar.average_class_vector(keyword_num)
+        res = tar.acc(keyword_num, rate, average_class_dic)
+        acc_list.append(round(res, 3))
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_title('关键词个数与准确率间的关系')
+    ax.set_xlabel('关键词个数')
+    ax.set_ylabel('准确率')
+    plt.plot(keyword_num_list, acc_list, color='black', markerfacecolor='r', marker='o')
+    for a, b in zip(keyword_num_list, acc_list):
+        plt.text(a, b, (a, b), ha='center', va='bottom', fontsize=10)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
