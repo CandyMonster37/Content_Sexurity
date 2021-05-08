e78d2cb9328999bfbcd335b48c2ae35498838780
CandyMonster37 <76767579+CandyMonster37@users.noreply.github.com>
2021-05-08 10:23:31 +0800

    update code

--- 实验3/task1/task1.py
+++ 实验3/task1/task1.py
@@ -3,7 +3,14 @@ import cv2 as cv
 from math import ceil
 
 
-def change(s_x, s_y, img, fill=False):
+def change(s_x, s_y, img, fill=False, save=False):
+    """
+    :param s_x: 宽度比例
+    :param s_y: 高度比例
+    :param img: 图片对象
+    :param fill: 窗口是否适应图片大小，默认为否
+    :param save: 是否保存结果，默认为否
+    """
     h, w = img.shape[:2]
     A1 = np.array([[s_x, 0, 0], [0, s_y, 0]], np.float32)
     if fill:
@@ -15,6 +22,8 @@ def change(s_x, s_y, img, fill=False):
     cv.imshow('original', img)
     cv.waitKey()
     cv.destroyAllWindows()
+    if save:
+        cv.imwrite('after.png', changed)
 
 
 def main():
@@ -22,7 +31,7 @@ def main():
     img = cv.imread(src, cv.IMREAD_COLOR)
     s_x = 0.5
     s_y = 0.5
-    change(s_x, s_y, img, True)
+    change(s_x, s_y, img, fill=True, save=False)
 
 
 if __name__ == '__main__':
