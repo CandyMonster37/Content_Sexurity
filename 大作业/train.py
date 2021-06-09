a9550ee7dfc382e9118b213b2aa21f326fefd66e
CandyMonster37 <76767579+CandyMonster37@users.noreply.github.com>
2021-06-09 11:47:24 +0800

    修改一些bug，删除文件保存，添加训练过程中AUC图像，添加图像预测

--- 大作业/train.py
+++ 大作业/train.py
@@ -3,16 +3,17 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from models import ResNet18, ResNet50
-from makedata import get_data_loader
+from makedata import *
 from tqdm import trange
 import numpy as np
 import os
-from evaluator import getAUC, getACC, save_results
+from evaluator import getAUC, getACC
 import datetime
 import matplotlib.pyplot as plt
+from PIL import Image
 
 
-def train(model, optimizer, loss_function, train_loader, device, num_class=2):
+def train(model, optimizer, loss_function, train_loader, device):
     model.train()
     for batch_idx, (inputs, targets) in enumerate(train_loader):
         optimizer.zero_grad()
@@ -93,15 +94,90 @@ def test(model, data_loader, device, auc_list, acc_list, mode='train', num_class
         acc_list.append(acc)
         print('{0} AUC: {1} ACC: {2}'.format(mode, round(auc, 5), round(acc, 5)))
 
-        # if output_root is not None:
-        #     output_dir = output_root
-        #     if not os.path.exists(output_dir):
-        #         os.mkdir(output_dir)
-        #     out_path = os.path.join(output_dir, '{0}.csv'.format(mode))
-        #     save_results(y_true, y_score, out_path)
+
+def detect(in_dir, model_dir, num_class=2):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    model = ResNet50(in_channels=3, num_classes=num_class).to(device)
+    model_loader = os.listdir(model_dir)
+    restore_model_path = os.path.join(model_dir, model_loader[0])
+    model.load_state_dict(torch.load(restore_model_path)['net'])
+
+    h = 56
+    w = 56
+
+    tar_dir = './to_check'
+    os.mkdir(tar_dir)
+    files = []
+    if os.path.isdir(in_dir):
+        all_files = os.listdir(in_dir)
+        for file in all_files:
+            ori = os.path.join(in_dir, file)
+            ori = os.path.abspath(ori)
+            tar = os.path.abspath(tar_dir)
+            tmp = os.popen('copy {0} {1}'.format(ori, tar))
+            files.append(file)
+    else:
+        tar = os.path.abspath(tar_dir)
+        ori = os.path.abspath(in_dir)
+        tmp = os.popen('mv {0} {1}'.format(ori, tar))
+        files.append(in_dir)
+
+    to_check_transform = transforms.Compose([
+        transforms.Resize([h, w]),  # resize到w * h大小
+        transforms.ToTensor(),  # 转化成Tensor
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+    for i in range(len(files)):
+        img_file = os.path.join(tar_dir, files[i])
+        img = Image.open(img_file).convert('RGB')
+        img = to_check_transform(img)
+        img = img.unsqueeze(0)
+        ans = predict(img, model, device)
+        print('{0} is recognized as class {1}'.format(files[i], int(ans)))
+    path = os.path.abspath('./to_check')
+    tmp = os.popen('rmdir /s/q {0}'.format(path))
 
 
-def main(in_dir, loader_dir, output_root, lr=0.001, end_epoch=100, batch_size=64, num_class=2):
+def predict(input, model, device):
+    model.to(device)
+    with torch.no_grad():
+        input = input.to(device)
+        out = model(input)
+        _, pre = torch.max(out.data, 1)
+        return pre.item()
+
+
+def show_res_imgs(end_epoch, train, test, dev, y_name, img_dir):
+    x_axis = []
+    for i in range(end_epoch):
+        x_axis.append(i + 1)
+
+    plt.figure(figsize=(10.8, 7.2), dpi=100)
+    # figsize=(a, b)，图像尺寸; dpi，分辨率，default=100; 最终图像大小(a*dip) : (b*dpi)
+    # eg: figsize=(10.8, 7.2), dpi=100; 最终图像大小1080 * 720像素
+
+    plt.plot(x_axis, train, color='red', label='train ' + y_name)
+    plt.plot(x_axis, test, color='green', label='test' + y_name)
+    plt.plot(x_axis, dev, color='skyblue', label='dev' + y_name)
+
+    plt.title('epochs = {0}'.format(str(end_epoch)))
+    plt.xlabel('epoch')
+    plt.ylabel(y_name)
+    plt.legend()
+
+    lim = int(end_epoch / 5) * end_epoch + 5
+    x_ticks = np.linspace(0, lim, 5)
+    plt.xticks(x_ticks)
+    y_ticks = np.arange(0, 1, 0.05)
+    plt.yticks(y_ticks)
+
+    plt.savefig(img_dir)
+    plt.show()
+
+
+def train_and_test(in_dir, loader_dir, output_root, lr=0.001, end_epoch=100, batch_size=64, num_class=2):
     start_epoch = 0
     val_auc_list = []
     train_auc_list = []
@@ -123,14 +199,14 @@ def main(in_dir, loader_dir, output_root, lr=0.001, end_epoch=100, batch_size=64
     print('Begin at: ', now)
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    # device = torch.device("cpu")
 
     model = ResNet50(in_channels=3, num_classes=num_class).to(device)
     loss_function = nn.CrossEntropyLoss()  # 设置损失函数
     optimizer = optim.Adam(model.parameters(), lr=lr)  # 设置优化器和学习率
 
     for epoch in trange(start_epoch, end_epoch):
-        train(model, optimizer, loss_function, train_loader, device, num_class=num_class)
+        print('\n')
+        train(model, optimizer, loss_function, train_loader, device)
         val(model, test_loader, device, val_auc_list, output_root, epoch, num_class=num_class)
         test(model, train_loader, device, auc_list=train_auc_list, acc_list=train_acc_list,
              mode='train', num_class=num_class)
@@ -141,6 +217,7 @@ def main(in_dir, loader_dir, output_root, lr=0.001, end_epoch=100, batch_size=64
 
     auc_list = np.array(val_auc_list)
     index = auc_list.argmax()
+
     now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print('Finish at: ', now)
     print('epoch {0} is the best model'.format(index))
@@ -163,54 +240,6 @@ def main(in_dir, loader_dir, output_root, lr=0.001, end_epoch=100, batch_size=64
                   y_name='auc value', img_dir='./auc_classes_{}.png'.format(str(num_class)))
     show_res_imgs(end_epoch, train_acc_list, test_acc_list, dev_acc_list,
                   y_name='acc value', img_dir='./acc_classes_{}.png'.format(str(num_class)))
-    # restore_model_path = os.path.join(output_root, tar_res)
-    # model.load_state_dict(torch.load(restore_model_path)['net'])
-
-
-def show_res_imgs(end_epoch, train, test, dev, y_name, img_dir):
-    x_axis = []
-    for i in range(end_epoch):
-        x_axis.append(i + 1)
-
-    plt.plot(x_axis, train, color='red', label='train ' + y_name)
-    plt.plot(x_axis, test, color='green', label='test' + y_name)
-    plt.plot(x_axis, dev, color='skyblue', label='dev' + y_name)
-
-    plt.title('epochs = {0}'.format(str(end_epoch)))
-    plt.xlabel('epoch')
-    plt.ylabel(y_name)
-    plt.legend()
-
-    lim = int(end_epoch / 5) * end_epoch + 5
-    x_ticks = np.linspace(0, lim, 5)
-    plt.xticks(x_ticks)
-    y_ticks = np.arange(0, 1, 0.05)
-    plt.yticks(y_ticks)
-
-    plt.figure(figsize=(12, 9), dpi=300)
-    # figsize，图像尺寸，默认为[6.4, 4.8], 640 * 480
-    # dpi，分辨率，默认为100.0
-
-    plt.savefig(img_dir)
-    plt.show()
-
-
-# def a_try(in_dir, loader_dir, output_root, batch_size=4, num_class=2):
-#     lr = 0.001
-#     train_loader, test_loader, dev_loader = \
-#         get_data_loader(in_dir=in_dir, out_dir=loader_dir, batch_size=batch_size)
-#     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-#     # device = torch.device("cpu")
-#
-#     model = ResNet50(in_channels=3, num_classes=num_class).to(device)
-#     # loss_function = nn.CrossEntropyLoss()  # 设置损失函数
-#     # optimizer = optim.Adam(model.parameters(), lr=lr)  # 设置优化器和学习率
-#
-#     restore_model_path = os.path.join(output_root, 'ckpt_21_auc_0.84517.pth')
-#     model.load_state_dict(torch.load(restore_model_path)['net'])
-#     test(model, train_loader, device, output_root=output_root, mode='train', num_class=num_class)
-#     test(model, dev_loader, device, output_root=output_root, mode='val', num_class=num_class)
-#     test(model, test_loader, device, output_root=output_root, mode='test', num_class=num_class)
 
 
 if __name__ == '__main__':
@@ -222,6 +251,20 @@ if __name__ == '__main__':
     end_epoch = 100
     batch_size = 8
     lr = 0.001
-    main(in_dir=in_dir, loader_dir=loader_dir, output_root=output_root,
+    train_and_test(in_dir=in_dir, loader_dir=loader_dir, output_root=output_root,
          end_epoch=end_epoch, batch_size=batch_size, lr=lr, num_class=num_class)
-    # a_try(in_dir, output_root, batch_size=4, num_class=num_class)
+    path = os.path.abspath(loader_dir)
+    tmp = os.popen('rmdir /s/q {0}'.format(path))
+
+    num_class = 2
+    in_dir = './data_' + str(num_class)
+    output_root = './ckpt_' + str(num_class)
+    loader_dir = './dataset_' + str(num_class)
+    train_and_test(in_dir=in_dir, loader_dir=loader_dir, output_root=output_root,
+         end_epoch=end_epoch, batch_size=batch_size, lr=lr, num_class=num_class)
+    path = os.path.abspath(loader_dir)
+    dele = os.popen('rmdir /s/q {0}'.format(path))
+
+    # in_dir = './tocheck'
+    # model_dir = './ckpt_4_yes'
+    # detect(in_dir, model_dir, num_class=4)
